3ef3ca59c55004321028b0f8ff9944afee15868d
Ayush <ayushmarkevens@gmail.com>
2024-01-20 20:55:28 -0800

    added sign language recognition project

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -12,16 +12,16 @@ const Projects = () => {
         <Row>
           {/* <Card className='shadow-lg rounded mb-3'>
             <Card.Body> */}
-              {projects_info.map((project) => {
-                return (
-                  <Col md={12} key={project.project_id}>
-                    <h5 className='csueb_red  mt-4 mr-3'>{project.project_name}</h5>
-                    <div className=' '>{project.desc}</div>
-                  </Col>
-                );
-              })}
-            {/* </Card.Body>
-          </Card> */}
+          {projects_info.map((project) => {
+            return (
+              <Col md={12} key={project.project_id}>
+                <h5 className='csueb_red  mt-4 mr-3'>{project.project_name}</h5>
+                {project.tech_stack && <div style={{ marginTop:"20px" }}><strong>Technology Stack :</strong> {project.tech_stack}</div>}
+                <div className='project_desc'>{project.desc}</div>
+                {project.link && <div style={{ lineHeight: 1.6 }}><strong>Take a look :</strong> <a className='csueb_red' href={project.link} target="_blank" rel="noopener noreferrer">Demo </a></div>}
+              </Col>
+            );
+          })}
         </Row>
       </Container>
     </div>
